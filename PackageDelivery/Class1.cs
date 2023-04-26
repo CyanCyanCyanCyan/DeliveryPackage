eb0f69462372445917b9eaef56e8b257aaf598e5
CyanCyanCyanCyan <cyano@live.wsd1.org>
2023-04-26 09:45:28 -0500

    ?

---
+++ PackageDelivery/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Calculations
+{
+	private double _weight;
+	private double _height;
+	private double _length;
+	private double _width;
+	
+	public Calculations(double weight, double height, double length, double width)
+	{
+		_weight=weight;
+		_height=height;
+		_length=length;
+		_width=width;
+	}
+}
---
+++ PackageDelivery/PackageDelivery/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace PackageDelivery 
+{ 
+    class Program 
+    {
+        public static void Main(string[] args)
+        {
+            Calculations c1 = new Calculations();
+            Console.WriteLine("Please enter the weight of your package in kg: ");
+            c1.Weight=Console.ReadLine();
+            Console.WriteLine("Please enter the height of your package in cm: ");
+            c1.Height=Console.ReadLine();
+            Console.WriteLine("Please enter the length of your package in cm: ");
+            c1.Length=Console.ReadLine();
+            Console.WriteLine("Please enter the width of your package in cm: ");
+            c1.Width=Console.ReadLine();
+        }
+    }
+}
+
+
+    
