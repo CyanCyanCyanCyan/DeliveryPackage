de2ff457f19dda111d8c177f8b520ff3045d26e1
Xyllera <132007984+Xyllera@users.noreply.github.com>
2023-05-04 10:15:39 -0500

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,42 @@
+﻿namespace PackageDelivery 
+{
+    class Program 
+    {
+        public static void Main(string[] args)
+        {
+            Calculations c1 = new Calculations();
+            Console.WriteLine("Please enter the weight of your package in kg: ");
+            c1.Weight=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the height of your package in cm: ");
+            c1.Height=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the length of your package in cm: ");
+            c1.Length=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the width of your package in cm: ");
+            c1.Width=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(c1.Description);
+            if(c1.Weight>29.9)
+            {
+                if(c1.Volume>100000)
+                {
+                    Console.WriteLine("Your package is too big, and too heavy.");
+                }
+                else
+                {
+                    Console.WriteLine("Your package is too heavy, but not too big.");
+                }
+            }
+            else
+            {
+                if(c1.Volume>100000)
+                {
+                    Console.WriteLine("Your package is too big, but not too heavy.");
+                }
+                else
+                {
+                    Console.WriteLine("Your package is good to go.");
+                }
+            }
+            
+        }
+    }
+}
---
+++ Class2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+public class Calculations
+{
+    private double _weight;
+    private double _height;
+    private double _length;
+    private double _width;
+    private double _volume;
+    public Calculations(double weight, double height, double length, double width, double _volume)
+    {
+        _weight = weight;
+        _height = height;
+        _length = length;
+        _width = width;
+        double volume = _height * _length * _width; ;
+        _volume = volume;
+    }
+    public double Weight
+    {
+        get => _weight;
+    }
+    public double Height
+    {
+        get => _height;
+    }
+    public double Length
+    {
+        get => _length;
+    }
+    public double Width
+    {
+        get => _width;
+    }
+    public double Volume
+    {
+        get => _volume;
+    }
+    public string Description
+    {
+        get => $"The weight is {Weight}kg. The height is {Height}cm. The length is {Length}cm. The width is {Width}cm. The volume is {Volume}cm³";
+    }
+
+}
